7056c032a6ea38cad3ce03665c8f1c0446349780
Carl Harris <ceharris@vt.edu>
2017-01-17 09:37:18 -0500

    fixed java7-ism

--- src/test/java/org/soulwing/jdbc/AbstractPreparedStatementExecutorTest.java
+++ src/test/java/org/soulwing/jdbc/AbstractPreparedStatementExecutorTest.java
@@ -67,7 +67,7 @@ public abstract class AbstractPreparedStatementExecutorTest
       }
     });
 
-    return newExecutor(psc, parameters).execute(dataSource);
+    return (T) newExecutor(psc, parameters).execute(dataSource);
   }
 
 }
